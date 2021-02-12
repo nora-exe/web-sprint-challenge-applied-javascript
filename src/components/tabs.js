c50e0954d34a539f1114c6ca318a9225ca01e706
Nora C <nocorser@gmail.com>
2021-02-12 16:53:23 -0600

    generated Topics tabs from API-provided topics

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,28 +1,56 @@
+// import axios
+
+import axios from 'axios'
+
 const Tabs = (topics) => {
   // TASK 3
-  // ---------------------
-  // Implement this function which takes an array of strings ("topics") as its only argument.
-  // As an example, if the topics passed are ['javascript', 'bootstrap', 'technology']
-  // then the function returns the markup below.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  //
-  // <div class="topics">
-  //   <div class="tab">javascript</div>
-  //   <div class="tab">bootstrap</div>
-  //   <div class="tab">technology</div>
-  // </div>
-  //
+  
+  /*
+  <div class="topics">
+    <div class="tab">javascript</div>
+    <div class="tab">bootstrap</div>
+    <div class="tab">technology</div>
+  </div>
+  */
+
+  // Create elements:
+
+  // Create "topics" (div) && define class
+
+  const topicsContainer = document.createElement('div');
+  topicsContainer.classList.add('topics');
+
+  // Create "tab" (div)
+
+  topics.forEach(topic => {
+    // for each topic: create, define class, set content (topic), append to topics container
+    const topicTab = document.createElement('div');
+    topicTab.classList.add('tab');
+    topicTab.textContent = topic;
+    topicsContainer.appendChild(topicTab);
+  })
+
+  // Return topics container
+
+  return topicsContainer;
 }
 
+// TASK 4
+
 const tabsAppender = (selector) => {
-  // TASK 4
-  // ---------------------
-  // Implement this function which takes a css selector as its only argument.
-  // It should obtain topics from this endpoint: `https://lambda-times-api.herokuapp.com/topics`
-  // Find the array of topics inside the response, and create the tabs using the Tabs component.
-  // Append the tabs to the element in the DOM that matches the selector passed to the function.
-  //
+
+  // Fetch array, pass in data to Tabs, append Tabs to 'selector' , && add error and success messages
+
+  axios.get(`https://lambda-times-api.herokuapp.com/topics`)
+  .then(res =>{
+    document.querySelector(selector).appendChild(Tabs(res.data.topics));
+  })
+  .catch(err => {
+    console.log(err, 'Oops! An error!');
+  })
+  .then(() => {
+    console.log('Success.');
+  })
 }
 
 export { Tabs, tabsAppender }
