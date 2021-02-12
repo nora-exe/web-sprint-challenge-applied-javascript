5ef46f7aaf9abbb57593bef0125d45fcd54e01b7
Nora C <nocorser@gmail.com>
2021-02-12 16:27:29 -0600

    generated header with title, date, and temperature

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,50 @@
 const Header = (title, date, temp) => {
   // TASK 1
-  // ---------------------
-  // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  //
-  //  <div class="header">
-  //    <span class="date">{ date }</span>
-  //    <h1>{ title }</h1>
-  //    <span class="temp">{ temp }</span>
-  //  </div>
-  //
+  
+  /*
+    <div class="header">
+      <span class="date">{ date }</span>
+      <h1>{ title }</h1>
+      <span class="temp">{ temp }</span>
+    </div>
+  */
+
+  // Create elements
+
+  const header = document.createElement('div');
+  const headerDate = document.createElement('span');
+  const headerTitle = document.createElement('h1');
+  const headerTemp = document.createElement('span');
+
+  // Define classes
+
+  header.classList.add('header');
+  headerDate.classList.add('date');
+  headerTemp.classList.add('temp');
+
+  // Add content
+
+  headerDate.textContent = date;
+  headerTitle.textContent = title;
+  headerTemp.textContent = temp; 
+
+  // Append elements
+
+  header.appendChild(headerDate);
+  header.appendChild(headerTitle);
+  header.appendChild(headerTemp);
+
+  // Return!
+
+  return header;
+
 }
 
+// TASK 2
+
 const headerAppender = (selector) => {
-  // TASK 2
-  // ---------------------
-  // Implement this function taking a css selector as its only argument.
-  // It should create a header using the Header component above, passing arguments of your choosing.
-  // It should append the header to the element in the DOM that matches the given selector.
-  //
+  // Select 'selector,' append Header to selector, pass in Header values
+  document.querySelector(selector).appendChild(Header('Lambda Times', 'February 12, 2021', '4°'));
 }
 
 export { Header, headerAppender }
-
-// test - initial commit
